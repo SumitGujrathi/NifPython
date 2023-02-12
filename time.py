b0c2273173bbd8278eaa27be01410dd32a7395fe
SumitGujrathi <112178227+SumitGujrathi@users.noreply.github.com>
2023-02-12 23:23:28 +0530

    Update time.py

--- time.py
+++ time.py
@@ -1,5 +1,9 @@
-        cur_time = datetime.now().strftime('%H:%M:%S')
-        cur_time = datetime.strptime(cur_time, '%H:%M:%S').time()
-        cur_time = cur_time
-        
-        print(cur_time)
+import datetime 
+
+cur_time = datetime.now().strftime('%H:%M:%S')
+
+cur_time = datetime.strptime(cur_time, '%H:%M:%S').time()
+
+cur_time = cur_time
+
+print(cur_time)
