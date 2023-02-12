63a627092e91b4ee9e92c70e0f19e8e863286664
SumitGujrathi <112178227+SumitGujrathi@users.noreply.github.com>
2023-02-12 15:30:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,30 @@ import pymongo
 from decouple import config
 
 
-# SECRET_KEY = config("SECRET_KEY")
+# List of Holidays Important *** Date formate should be like MM-DD-YYYY eg. 01-01-2023****
+
+OffDates = ["01-26-2023","03-07-2023","03-30-2023","04-04-2023","04-07-2023","04-14-2023","05-01-2023",
+"06-28-2023","08-15-2023","09-19-2023","10-02-2023","10-24-2023","11-14-2023","11-27-2023","12-25-2023",
+"01-01-2023","01-07-2023","01-08-2023","01-14-2023","01-15-2023","01-21-2023","01-22-2023","01-28-2023",
+"01-29-2023","02-11-2023","02-18-2023","02-19-2023","02-25-2023",
+"02-26-2023","03-04-2023","03-05-2023","03-11-2023","03-12-2023","03-18-2023","03-19-2023","03-25-2023",
+"03-26-2023","04-01-2023","04-02-2023","04-08-2023","04-09-2023","04-15-2023","04-16-2023","04-22-2023",
+"04-23-2023","04-29-2023","04-30-2023","05-06-2023","05-07-2023","05-13-2023","05-14-2023","05-20-2023",
+"05-21-2023","05-27-2023","05-28-2023","06-03-2023","06-04-2023","06-10-2023","06-11-2023","06-17-2023",
+"06-18-2023","06-24-2023","06-25-2023","07-01-2023","07-02-2023","07-08-2023","07-09-2023","07-15-2023",
+"07-16-2023","07-22-2023","07-23-2023","07-29-2023","07-30-2023","08-05-2023","08-06-2023","08-12-2023",
+"08-13-2023","08-19-2023","08-20-2023","08-26-2023","08-27-2023","09-02-2023","09-03-2023","09-09-2023",
+"09-10-2023","09-16-2023","09-17-2023","09-23-2023","09-24-2023","09-30-2023","10-01-2023","10-07-2023",
+"10-08-2023","10-14-2023","10-15-2023","10-21-2023","10-22-2023","10-28-2023","10-29-2023","11-04-2023",
+"11-05-2023","11-11-2023","11-12-2023","11-18-2023","11-19-2023","11-25-2023","11-26-2023","12-02-2023",
+"12-03-2023","12-09-2023","12-10-2023","12-16-2023","12-17-2023","12-23-2023","12-24-2023","12-30-2023","12-31-2023"
+]
+
+# NSE Data Pull + Data Clearing + MongoDB Funciton
+
+
+SECRET_KEY = config("SECRET_KEY")
+
 
 def Pull_Chain_Data(Symbol):
 
@@ -17,8 +40,8 @@ def Pull_Chain_Data(Symbol):
     Dataset_Name = "NSE"
     Collection = Symbol
 
-    linkMD = f"mongodb+srv://sumitgujrathi24:8lpVTDIBtyHKWRH3@sumit.ybjewjm.mongodb.net/{Dataset_Name}?retryWrites=true&w=majority"
-#     linkMD = SECRET_KEY
+    # linkMD = f"mongodb+srv://sumitgujrathi24:8lpVTDIBtyHKWRH3@sumit.ybjewjm.mongodb.net/{Dataset_Name}?retryWrites=true&w=majority"
+    linkMD = SECRET_KEY
 
     Client = pymongo.MongoClient(linkMD, 27017)
 
@@ -87,4 +110,132 @@ def Pull_Chain_Data(Symbol):
         pass
 
 
-Pull_Chain_Data("NIFTY")
+# Date Setting 
+
+Today_Date = datetime.now().strftime('%m-%d-%Y')
+print(Today_Date)
+
+
+# On_Day = Today_Date in OffDates # If False Market is On.
+On_Day = Today_Date in OffDates
+
+print(On_Day)
+
+
+# Start Time Setting 
+
+cur_time = datetime.now().strftime('%H:%M:%S')
+cur_time = datetime.strptime(cur_time, '%H:%M:%S').time()
+print(cur_time)
+
+
+# Start Time Setting
+
+Start_time = '10:05:00' # time on web 09:17:00 in india
+Start_time = datetime.strptime(Start_time, '%H:%M:%S').time()
+
+
+# Stop Time Setting 
+
+Stop_time = '10:30:00'   # time on web 15:28:00 in india
+Stop_time = datetime.strptime(Stop_time, '%H:%M:%S').time()
+
+
+restart = True
+# def date_input():
+#     if (Today_Date == input()):
+#         restart = True
+
+while restart:
+
+    if (On_Day == False):
+
+        # Date Setting 
+        Today_Date = datetime.now().strftime('%m-%d-%Y')
+        Today_Date = Today_Date
+        On_Day = Today_Date in OffDates
+        
+        restart = True
+
+        while restart:
+
+            if(cur_time > Start_time and cur_time < Stop_time):
+                restart = True
+                # Current Time Setting 
+                cur_time = datetime.now().strftime('%H:%M:%S')
+                cur_time = datetime.strptime(cur_time, '%H:%M:%S').time()
+                cur_time = cur_time
+
+                # Main Script here below =>
+
+
+                Pull_Chain_Data("NIFTY")
+                Pull_Chain_Data("BANKNIFTY")
+
+
+
+
+                # <= Main Script End =>
+                print(cur_time)
+                print("Pulling")
+                time.sleep(240)
+                
+            else:
+                # Current Time Setting
+                cur_time = datetime.now().strftime('%H:%M:%S')
+                cur_time = datetime.strptime(cur_time, '%H:%M:%S').time()
+                cur_time = cur_time
+
+
+                print("Time Over")
+                print(cur_time)
+                restart = "Wait"      
+
+                Today_Date = datetime.now().strftime('%m-%d-%Y')
+                Today_Date = Today_Date    
+                On_Day = Today_Date in OffDates
+                # On_Day = input() in OffDates
+
+
+                if (On_Day == False):
+                # Date Setting 
+
+                    Today_Date = datetime.now().strftime('%m-%d-%Y')
+                    Today_Date = Today_Date
+                    On_Day = Today_Date in OffDates
+
+                    time.sleep(1)
+                    restart = "Wait"
+                time.sleep(50)
+                break
+
+                # restart = False
+
+    else:
+
+        cur_time = datetime.now().strftime('%H:%M:%S')
+        cur_time = datetime.strptime(cur_time, '%H:%M:%S').time()
+        cur_time = cur_time
+
+
+        print("Off Day")
+        print(cur_time)
+        restart = "Wait"
+        Today_Date = datetime.now().strftime('%m-%d-%Y')
+        Today_Date = Today_Date    
+        On_Day = Today_Date in OffDates
+        # On_Day = input() in OffDates
+      
+        if (On_Day == True):
+        # Date Setting 
+
+            Today_Date = datetime.now().strftime('%m-%d-%Y')
+            Today_Date = Today_Date
+            On_Day = Today_Date in OffDates
+
+            time.sleep(1)
+            restart = True
+        time.sleep(43200)         
+
+
+
