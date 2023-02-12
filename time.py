0ad9380ae94c9fd4f4926e7ed9fb0e76f2d65847
SumitGujrathi <112178227+SumitGujrathi@users.noreply.github.com>
2023-02-12 23:11:49 +0530

    Create time.py

---
+++ time.py
@@ -0,0 +1,5 @@
+        cur_time = datetime.now().strftime('%H:%M:%S')
+        cur_time = datetime.strptime(cur_time, '%H:%M:%S').time()
+        cur_time = cur_time
+        
+        print(cur_time)
